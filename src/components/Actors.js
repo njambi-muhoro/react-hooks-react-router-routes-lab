65a998d0fd218b149a29d3c8e350ebf658897325
+ <+>
2023-01-18 01:19:24 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    <h1>Actors Page</h1>
+    <div>
+        {
+          actors.map((actor, index)=>(
+            <ul key={index}>
+              {actor.name}
+              <li>{actor.movies}</li>
+            </ul>
+            
+          ))
+        }
+     </div>
+
+
+  </div>);
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+      <h1>Movies Page</h1>
+      <div>
+        {
+          movies.map((movie, index)=>(
+          <div key={index}>
+            <h2 >"Name:" {movie.title}</h2>
+            <h4 >"Time:" {movie.time}</h4>
+            <ul >
+              Genres
+            <li>{movie.genres}</li>
+            </ul>
+          </ div>
+          ))
+        }
+     </div>
+
+  </div>)
 }
 
 export default Movies;
